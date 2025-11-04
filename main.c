769adfa6c262a152c39a7cda155e92cd890d7cb9
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-11-04 19:32:22 -0300

    rascunho do menu na função main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int questao;
+  printf("------------------------------\n");
+  printf("escolha uma questao: de 1 a 28\n");
+  printf("------------------------------\n");
+  scanf("%d", &questao);
+
+  return 0;
+}
