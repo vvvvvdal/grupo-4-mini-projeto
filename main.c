e2ef995dd7aff01b35697f9dbfaddc4ed270a038
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2025-11-05 09:30:00 -0300

    questões 7, 14, 21, 28 feitas e rascunho do menu melhorado

--- main.c
+++ main.c
@@ -1,11 +1,168 @@
 #include <stdio.h>
 
-int main() {
-  int questao;
-  printf("------------------------------\n");
-  printf("escolha uma questao: de 1 a 28\n");
-  printf("------------------------------\n");
-  scanf("%d", &questao);
+void q7_fatorial();
+void q14_somatorio_invervalo();
+void q21_maior_que();
+void q28_nor();
 
-  return 0;
+int main() {
+    int questao = -1;
+
+    while(questao != 0) {
+        printf("----------------------------------\n");
+        printf("--escolha uma questao: de 1 a 28--\n");
+        printf("(digite 0 para encerrar o programa)\n");
+        printf("----------------------------------\n");
+        printf("opcao: ");
+        scanf("%d", &questao);
+        printf("\n");
+
+        switch (questao) {
+            case 0:
+                printf("encerrando o programa...\n");
+                break;
+            case 1:
+                printf("executando a questao 1...\n");
+                break;
+            case 2:
+                printf("executando a questao 2...\n");
+                break;
+            case 3:
+                printf("executando a questao 3...\n");
+                break;
+            case 4:
+                printf("executando a questao 4...\n");
+                break;
+            case 5:
+                printf("executando a questao 5...\n");
+                break;
+            case 6:
+                printf("executando a questao 6...\n");
+                break;
+            case 7:
+                printf("executando a questao 7...\n");
+                q7_fatorial();
+                break;
+            case 8:
+                printf("executando a questao 8...\n");
+                break;
+            case 9:
+                printf("executando a questao 9...\n");
+                break;
+            case 10:
+                printf("executando a questao 10...\n");
+                break;
+            case 11:
+                printf("executando a questao 11...\n");
+                break;
+            case 12:
+                printf("executando a questao 12...\n");
+                break;
+            case 13:
+                printf("executando a questao 13...\n");
+                break;
+            case 14:
+                printf("executando a questao 14...\n");
+                q14_somatorio_invervalo();
+                break;
+            case 15:
+                printf("executando a questao 15...\n");
+                break;
+            case 16:
+                printf("executando a questao 16...\n");
+                break;
+            case 17:
+                printf("executando a questao 17...\n");
+                break;
+            case 18:
+                printf("executando a questao 18...\n");
+                break;
+            case 19:
+                printf("executando a questao 19...\n");
+                break;
+            case 20:
+                printf("executando a questao 20...\n");
+                break;
+            case 21:
+                printf("executando a questao 21...\n");
+                q21_maior_que();
+                break;
+            case 22:
+                printf("executando a questao 22...\n");
+                break;
+            case 23:
+                printf("executando a questao 23...\n");
+                break;
+            case 24:
+                printf("executando a questao 24...\n");
+                break;
+            case 25:
+                printf("executando a questao 25...\n");
+                break;
+            case 26:
+                printf("executando a questao 26...\n");
+                break;
+            case 27:
+                printf("executando a questao 27...\n");
+                break;
+            case 28:
+                printf("executando a questao 28...\n");
+                q28_nor();
+                break;
+        }
+
+    }
+
+    return 0;
+}
+
+void q7_fatorial() {
+    int n, i;
+    long long int fatorial = 1;
+
+    scanf("%d !", &n);
+
+    for(i = n; i > 0; i--) {
+        fatorial *= i;
+    }
+
+    printf("%lld\n", fatorial);
+}
+
+void q14_somatorio_invervalo() {
+    int a, b, i;
+    long long int soma = 0;
+    scanf("%d S %d", &a, &b);
+
+    for(i = a; i <= b; i++) {
+        soma += i;
+    }
+
+    printf("%lld\n", soma);
+}
+
+void q21_maior_que() {
+    int a, b, ehMaior = 0;
+    scanf("%d > %d", &a, &b);
+
+    if(a > b) {
+        ehMaior = !ehMaior;
+    }
+
+    if(ehMaior) {
+        printf("SIM\n");
+    } else {
+        printf("NAO\n");
+    }
+}
+
+void q28_nor() {
+    int a, b;
+    scanf("%d !| %d", &a, &b);
+
+    if(a == 0 && b == 0) {
+        printf("SIM\n");
+    } else {
+        printf("NAO\n");
+    }
 }
